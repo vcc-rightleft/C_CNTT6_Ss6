2743dcd37f542023ce4d3543f0f9651acc0658a0
vcc-rightleft <trumfaifai00ngo@gmail.com>
2025-08-04 14:09:59 +0700

    Update C_CNTT6_Ss6_Ex10.c

--- C_CNTT6_Ss6_Ex10.c
+++ C_CNTT6_Ss6_Ex10.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main() {
-    int number;
+int main(){
+    int number,newNumber;
     printf("Moi nhap vao so nguyen bat ky: ");
     scanf("%d", &number);
-    int number1= abs(number);
-    while (number1 != 0) {
-        int sum=number1%10;
-        printf(":%d\n",sum);
-        number1/=10;
+    while(number != 0) {
+        int sum= number%10;
+        newNumber=newNumber*10+sum;
+        number=number/10;
+    }
+    while (newNumber != 0) {
+        int sum= newNumber%10;
+        printf("%d \n", sum);
+        newNumber=newNumber/10;
     }
 }
